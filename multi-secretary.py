9b46e95e8d7aa6439044327c9191ecec5694e9d7
Daniel <daniel.sloredo@gmail.com>
2024-06-27 14:21:02 -0400

    function to approximate the dynamic problem with the deterministic relaxation

--- multi-secretary.py
+++ multi-secretary.py
@@ -16,6 +16,15 @@ def generate_vectors(n):
     vectors = np.array(grid).T.reshape(-1, n)
     return vectors
 
+def deterministic_msecretary(probabilities, rewards, n_types, t, x):
+    y = cp.Variable(n_types)
+    objective = cp.Maximize(cp.sum(rewards @ y))
+    constraints = [0<=y, y<= probabilities*t, cp.sum(y)<=x]
+    prob = cp.Problem(objective, constraints)
+    result = prob.solve()
+
+    return result
+
 def msecretary(val, sol_index, prob_choice, rewards, t, x): 
     if t == 0 or x == 0:
         return 0 
@@ -30,23 +39,7 @@ def msecretary(val, sol_index, prob_choice, rewards, t, x):
     
     return val[t][x]
 
-def deterministic_msecretary(probabilities, rewards, n_types, t, x):
-    y = cp.Variable(n_types)
-    objective = cp.Maximize(cp.sum(rewards @ y))
-    constraints = [0<=y, y<= probabilities*t, cp.sum(y)<=x]
-    prob = cp.Problem(objective, constraints)
-    result = prob.solve()
-    print(y.value)
-
-    return result
-
-def dynamic_solution(n_types, T, capacity):
-    probabilities = uniform.rvs(size = n_types)
-    probabilities /= probabilities.sum()
-    rewards = uniform.rvs(scale = 10, size = n_types)
-
-    vectors = generate_vectors(n_types)
-    prob_choice = vectors * probabilities #p_i * u_i where u_i are binary variables.
+def dynamic_solution(T, capacity):
     
     val = np.zeros((T+1, capacity+1))
     sol_index = np.zeros((T+1, capacity+1), dtype=int)
@@ -56,13 +49,45 @@ def dynamic_solution(n_types, T, capacity):
 
     return result, val, sol 
 
+def approx_dynamic_solution(T, capacity):
+
+    val = np.zeros((T+1, capacity+1))
+    sol_index = np.zeros((T+1, capacity+1), dtype=int)
+
+    for t in range(1, T+1):
+        for x in range(1, capacity+1):
+            if t == 0 or x == 0:
+                val[t][x] = 0 
+    
+            q_val = (np.sum(prob_choice * (rewards + deterministic_msecretary(probabilities, rewards, n_types, t-1, x-1)), axis = 1) 
+                     + (1-prob_choice.sum(axis = 1))*deterministic_msecretary(probabilities, rewards, n_types, t-1, x))
+            sol_index[t][x] = np.argmax(q_val)
+            val[t][x] = q_val[sol_index[t][x]]
+
+    result = val[T][capacity]
+    sol = vectors[sol_index]
+
+    return result, val, sol
+
+
 if __name__ == '__main__':
     np.random.seed(42)
-    n_types = 3
+    n_types = 4
     capacity = 5 #capacity. benchmark = 5
     T = 10 #Time periods remaining. benchmark = 10
-    result_dynamic, val_dynamic, sol_dynamic = dynamic_solution(n_types, T, capacity)
+
+    probabilities = uniform.rvs(size = n_types)
+    probabilities /= probabilities.sum()
+    rewards = uniform.rvs(scale = 10, size = n_types)
+
+    vectors = generate_vectors(n_types)
+    prob_choice = vectors * probabilities #p_i * u_i where u_i are binary variables.
+
+    result_dynamic, val_dynamic, sol_dynamic = dynamic_solution(T, capacity)
     print(result_dynamic, val_dynamic, sol_dynamic)
+
+    result_approx, val_approx, sol_approx = approx_dynamic_solution(T, capacity)
+    print(result_approx, val_approx, sol_approx)
 
     #print(deterministic_msecretary(probabilities, rewards, n_types, T, capacity))
     
